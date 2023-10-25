d8037b366384d51179ce3a3a41de52469daf904b
I-Mahfouf <143210485+I-Mahfouf@users.noreply.github.com>
2023-10-25 16:16:01 +0200

    Étape 1.2 : Réalisation du filtre des travaux
    - Création de fonctions asynchrones (modifications).
    
    - Modification HTML :
    -> ajout des /div qui vont contenir les boutons et travaux crées dynamiquement.
    
    Bonus :
    - Ajout des chemins vers la page "login" et inversement.
    
    = S'ajoute la possibilité de filtrer la galerie par catégorie de projet.

--- FrontEnd/fetch-api.js
+++ FrontEnd/fetch-api.js
@@ -1,24 +1,108 @@
-fetch("http://localhost:5678/api/works")
-.then(response => response.json())
-.then(responseApi => {
-    console.table(responseApi);
+//* Déclaration des variables *//
+let worksData; //* Stock les données que j'obtiens via l'API *//
+let categories; //* Ensemble pour stocker les catégories *//
+let gallery;
 
-    const gallery = document.querySelector('.gallery');
+//* Fonction - Récupération des travaux *//
+async function getWorks() {
+  const response = await fetch("http://localhost:5678/api/works");
+  worksData = await response.json();
+  console.table(worksData);
 
-    responseApi.forEach(works => {
-        const figureElement = document.createElement('figure');
+  gallery = document.querySelector('.gallery');
 
-        const imgElement = document.createElement ('img');
-        imgElement.src = works.imageUrl;
+  worksData.forEach(work => {
+    displayWork(work);
+  });
+}
 
-        const figcaptionElement = document.createElement ('figcaption');
-        figcaptionElement.textContent = works.title;
+//* Fonction - Récupération des catégories et création des filtres *//
+async function getCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const fetchedCategories = await response.json();
+  console.table(fetchedCategories);
 
-        figureElement.appendChild(imgElement);
-        figureElement.appendChild(figcaptionElement);
+  const filterContainer = document.querySelector('#filter-container');
+  filterContainer.innerHTML = '';
 
-        gallery.appendChild(figureElement);
+  //* Création du bouton "Tous" *//
+  const allBtn = document.createElement("button");
+  allBtn.classList.add("btn");
+  allBtn.innerText = "Tous";
+  allBtn.id = "all";
+  filterContainer.appendChild(allBtn);
+
+  allBtn.addEventListener('click', (event) => {
+    const categoryId = event.target.id;
+    console.log(categoryId);
+    filterWorksByCategoryId(categoryId);
+    setActiveFilter(categoryId);
+  });
+
+  categories = new Set();
+
+  fetchedCategories.forEach(category => {
+    const filtersBtn = document.createElement("button");
+    filtersBtn.classList.add("btn");
+    filtersBtn.innerText = category.name;
+    filtersBtn.id = category.id;
+    filterContainer.appendChild(filtersBtn);
+
+    //* Ajoutez la catégorie à l'ensemble categories *//
+    categories.add(category.name);
+
+    filtersBtn.addEventListener('click', (event) => {
+      const categoryId = event.target.id;
+      console.log(categoryId);
+      filterWorksByCategoryId(categoryId);
     });
-})
+  });
+}
 
+//* Fonction pour filtrer les travaux *//
+function filterWorksByCategoryId(categoryId) {
+  console.log(categoryId);
+  
+  gallery = document.querySelector('.gallery'); 
+  gallery.innerHTML = '';
 
+  if (categoryId == "all") {
+    console.log("all");
+    worksData.forEach(work => {
+      displayWork(work);
+    });
+  } else {
+    console.log("un autre message");
+    worksData.forEach(work => {
+      console.log(work);
+      if (categoryId == work.categoryId) {
+        console.log("Message erreur");
+        displayWork(work);
+      }
+    });
+  }
+}
+
+//* Fonction pour créer les éléments HTML *//
+function displayWork(work) {
+  const figureElement = document.createElement('figure');
+  const imgElement = document.createElement('img');
+  imgElement.src = work.imageUrl;
+
+  const figcaptionElement = document.createElement('figcaption');
+  figcaptionElement.textContent = work.title;
+
+  figureElement.appendChild(imgElement);
+  figureElement.appendChild(figcaptionElement);
+
+  gallery.appendChild(figureElement);
+}
+
+//* Fonction d'ajout de la classe "active" *//
+
+document.addEventListener("DOMContentLoaded", () => {
+  getCategories();
+  getWorks().then(() => {
+  filterWorksByCategoryId("all");
+  });
+});
